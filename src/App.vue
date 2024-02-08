75b1561290167dfc241f68046a699cb525794519
mielecarmine <mielecarmine993@gmail.com>
2024-02-08 16:39:26 +0100

    add: components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script>
-export default {};
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  components: { AppHeader, AppMain, AppFooter },
+};
 </script>
 
 <template></template>
