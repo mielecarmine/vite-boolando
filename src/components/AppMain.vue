2095f50bf0e211331e4a587816adb3c334ae770d
mielecarmine <mielecarmine993@gmail.com>
2024-02-12 20:52:02 +0100

    fix: img path

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,24 +1,152 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          frontImage: "1.webp",
+          backImage: "1b.webp",
+          brand: "Levi's",
+          name: "Relaxed Fit",
+          price: 29.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 2,
+          frontImage: "2.webp",
+          backImage: "2b.webp",
+          brand: "Guess",
+          name: "Roses Tee",
+          price: 20.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "discount",
+              value: "-30%",
+            },
+          ],
+        },
+        {
+          id: 3,
+          frontImage: "3.webp",
+          backImage: "3b.webp",
+          brand: "Come Zucchero Filato",
+          name: "Voglia di colori pastello",
+          price: 129.99,
+          isInFavorites: false,
+          badges: [
+            {
+              type: "discount",
+              value: "-30%",
+            },
+          ],
+        },
+        {
+          id: 4,
+          frontImage: "4.webp",
+          backImage: "4b.webp",
+          brand: "Levi's",
+          name: "Tee Unisex",
+          price: 14.99,
+          isInFavorites: false,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 5,
+          frontImage: "5.webp",
+          backImage: "5b.webp",
+          brand: "Maya Deluxe",
+          name: "Stripe Bodice",
+          price: 99.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+            {
+              type: "discount",
+              value: "-50%",
+            },
+          ],
+        },
+        {
+          id: 6,
+          frontImage: "6.webp",
+          backImage: "6b.webp",
+          brand: "Esprit",
+          name: "Maglione - Black",
+          price: 29.99,
+          isInFavorites: true,
+          badges: [
+            {
+              type: "tag",
+              value: "Sostenibilità",
+            },
+          ],
+        },
+      ],
+    };
+  },
+
+  methods: {
+    buildImagePath(imageName) {
+      const imageUrl = new URL("../assets/img/" + imageName, import.meta.url);
+      return imageUrl.href;
+    },
+  },
+};
 </script>
 
 <template>
   <div class="container">
     <div class="flex">
-      <div class="cards">
-        <img class="first_img" src="../img/1.webp" alt="" />
-        <img class="second_img" src="../img/1b.webp" alt="" />
+      <div v-for="product in products" class="cards">
+        <img
+          class="first_img"
+          :src="buildImagePath(product.frontImage)"
+          alt=""
+        />
+        <img
+          class="second_img"
+          :src="buildImagePath(product.backImage)"
+          alt=""
+        />
         <p class="heart">&hearts;</p>
         <p class="sale">
-          <span class="cut-off">-50%</span>
-          <span class="sustenaibl">Sostenibilità</span>
+          <span :class="product.badges.type == 'discount' ? 'cut-off' : ''">{{
+            product.badges.value
+          }}</span>
+          <span :class="product.badges.type == 'tag' ? 'tag' : ''">{{
+            product.badges.value
+          }}</span>
         </p>
         <div class="bottom_text">
-          <span>Levi's</span><br />
-          <b>RELAXED FIT TEE UNISEX</b>
+          <span>{{ product.brand }}</span
+          ><br />
+          <b>{{ product.name }}</b>
           <p>
-            <span class="new_price">14,99 $</span>
-            <span class="old_price">29,99 $</span>
+            <span class="new_price">{{ product.price }}</span>
           </p>
         </div>
       </div>
@@ -67,7 +195,7 @@ export default {};
   margin-right: 3px;
 }
 
-.sustenaibl {
+.tag {
   background-color: green;
   padding: 5px 15px;
   color: white;
@@ -79,11 +207,6 @@ export default {};
 }
 
 .new_price {
-  color: red;
   font-weight: bold;
-}
-
-.old_price {
-  text-decoration: line-through;
 }
 </style>
